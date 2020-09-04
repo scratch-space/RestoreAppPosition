5535b6437556f31182e0cbd856cb4d1b0956be7b
Vatsan Madhavan <vatsan.madhavan@microsoft.com>
2020-09-04 15:46:03 -0700

    Initial Prroject onboarding

---
+++ RestoreAppPositon/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+
+namespace RestoreAppPositon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            if (Settings.Default.HasStartupPosition)
+            {
+                var startupPos = Settings.Default.StartupPosition;
+                Left = startupPos.X;
+                Top = startupPos.Y;
+            }
+            InitializeComponent();
+        }
+
+        private void OnClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            SaveWindowPosition();
+        }
+
+        private void SaveWindowPosition()
+        {
+            try
+            {
+                Settings.Default.StartupPosition = new Point(Left, Top);
+                Settings.Default.HasStartupPosition = true;
+                Settings.Default.Save();
+            }
+            catch
+            {
+            }
+        }
+    }
+}
